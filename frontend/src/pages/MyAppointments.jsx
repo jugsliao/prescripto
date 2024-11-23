7f4927ccfed96e3b493b62fcca667fa7efb00205
jacques <jacquesliaozeli@hotmail.com>
2024-11-23 11:54:33 +0800

    myappointment and myprofile page edits

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const MyAppointments = () => {
+
+  const { doctors } = useContext(AppContext)
   return (
     <div>
-      
+      <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My appointments</p>
+      <div>
+        {doctors.slice(0, 2).map((item, index) => (
+          <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
+            <div>
+              <img src={item.image} alt="" />
+            </div>
+            <div>
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+              <p>Address:</p>
+              <p>{item.address.line1}</p>
+              <p>{item.address.line2}</p>
+              <p><span>Date & Time:</span>25, July 2024 | 8:30pm</p>
+            </div>
+            <div></div>
+            <div>
+              <button>Pay Online</button>
+              <button>Cancel appointment</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
